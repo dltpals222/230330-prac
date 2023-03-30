ea51d780a9358b5f16f3a35c548fc40b581bea99
leesemin <leesemin222@gmail.com>
2023-03-30 18:50:44 +0900

    클래스함수 사용해서 서버 구동시켜보기(작성법연습용)

---
+++ serverPrac/app2.js
@@ -0,0 +1,21 @@
+import http from 'http'
+import url from 'url'
+import fs from 'fs'
+import path from 'path'
+
+// class Server {
+//   constructor(port) {
+//     this.port = port;
+//   }
+
+//   start(){
+//     http.createServer((req, res)=>{
+//       const parsedUrl = url.parse(req.url, true);
+//       const pathName = parsedUrl.pathname;
+//       const method = req.method;
+
+//       if (method === 'get' && pathName === '/')
+//     })
+//   }
+
+// } //class server끝
