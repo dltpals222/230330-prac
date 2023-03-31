fcabfadaa1b6c172ab94da9c607475977fe84325
leesemin <leesemin222@gmail.com>
2023-03-31 22:36:43 +0900

    집에서 작성법연습

--- serverdustmq/app.js
+++ serverdustmq/app.js
@@ -5,11 +5,34 @@ import path from 'path';
 
 class Server {
   constructor(port){
-    const port = port
+    this.port = port
   }
 
   start(){
-    
+    http.createServer((req, res) => {
+      const reqUrl = url.parse(req.url,true);
+      const pathName = reqUrl.pathname;
+      const method = req.method;
+
+      if(pathName === '/' && method === 'get'){
+        fs.readFile('/index.html',err => { 
+          if(err){
+            res.writeHead(500, {'content-type':'text/plain'})
+            res.wirte('500 서버에 문제가 있습니다.')
+            res.end(console.log('500 서버에 문제가 있습니다.'))
+          } else {
+            res.writeHead(200, {'content-type':'text/html'})
+            
+          }
+        })
+        res.writeHead(200,{'content-type':'text/html'})
+        res.end(console.log(`${this.port} 가 정상작동합니다`))
+      } else if(pathName === '/' && method === 'post') {
+        let body = '';
+      }
+
+    })
+    console.dir(reqUrl)
   }
   
 }
