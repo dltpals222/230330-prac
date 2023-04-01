c29375ea777282f87bb4fe307b63c24f1452e63d
leesemin <leesemin222@gmail.com>
2023-04-01 18:46:33 +0900

    서버 작동 안되는 이유 2가지 있었음 자세한 사항은 notion의 노력페이지에서 확인할 것

--- serverdustmq/app.js
+++ serverdustmq/app.js
@@ -1,7 +1,7 @@
 import http from 'http'
 import url from 'url'
 import fs from 'fs'
-import path from 'path';
+import path, { resolve } from 'path';
 
 class Server {
   constructor(port){
@@ -14,25 +14,33 @@ class Server {
       const pathName = reqUrl.pathname;
       const method = req.method;
 
-      if(pathName === '/' && method === 'get'){
-        fs.readFile('/index.html',err => { 
+      if(pathName === '/' && method === 'GET'){
+        fs.readFile(path.join(resolve(),'/index.html'),(err, data) => { 
           if(err){
             res.writeHead(500, {'content-type':'text/plain'})
-            res.wirte('500 서버에 문제가 있습니다.')
-            res.end(console.log('500 서버에 문제가 있습니다.'))
+            res.end('500 서버에 문제가 있습니다.')
+            console.error(err)
           } else {
             res.writeHead(200, {'content-type':'text/html'})
-            
+            res.write(data)
+            res.end()
           }
         })
-        res.writeHead(200,{'content-type':'text/html'})
-        res.end(console.log(`${this.port} 가 정상작동합니다`))
-      } else if(pathName === '/' && method === 'post') {
+      } else if(pathName === '/' && method === 'POST') {
         let body = '';
-      }
+        req.on('data',chunk => {
+          body += chunk.toString();
+        }) //req.on data끝
+        req.on('end',()=> {
+          console.log(body)
+          res.writeHead(200, {'content-type':'text/html charset=utf8'});
+          res.write(`<h1>${body}</h1>`)
+          res.end();
+        }) //req.on end 끝
+      } //post 끝
+    }).listen(this.port,console.log(`${this.port} 정상작동 합니다.`)) // createserver 끝
+  } //start() 종료
+} //class server 끝
 
-    })
-    console.dir(reqUrl)
-  }
-  
-}
+const server = new Server(2080);
+server.start();
